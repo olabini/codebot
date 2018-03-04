ec034182bd1f25f5992fcfa955d4bde5b1bdf34a
Janik Rabe <info@janikrabe.com>
2018-03-04 15:52:07 +0200

    Add webhook signature verification

--- lib/codebot/cryptography.rb
+++ lib/codebot/cryptography.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'openssl'
 require 'securerandom'
 
 module Codebot
@@ -16,8 +17,21 @@ module Codebot
     # Generates a random webhook secret.
     #
     # @return [String] the generated secret
-    def self.generate_secret(len = 32)
+    def self.generate_secret(len = nil)
       SecureRandom.base64(len || 32)
+    end
+
+    # Verifies a webhook signature.
+    #
+    # @param body [String] the webhook body
+    # @param secret [String] the correct secret
+    # @param signature [String] the signature to verify
+    # @return [Boolean] whether the signature is correct
+    def self.valid_signature?(body, secret, signature)
+      return false if signature.nil?
+      digest = OpenSSL::Digest.new 'sha1'
+      good_signature = 'sha1=' + OpenSSL::HMAC.hexdigest(digest, secret, body)
+      Rack::Utils.secure_compare good_signature, signature
     end
   end
 end
