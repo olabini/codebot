07fe372246bd13c6c48a39fd4fbc6e86172b2a04
Janik Rabe <35016218+janikrabe@users.noreply.github.com>
2018-03-08 17:56:23 +0200

    Allow formatters to return multiple messages

--- lib/codebot/formatters.rb
+++ lib/codebot/formatters.rb
@@ -7,27 +7,25 @@ require 'codebot/formatters/ping'
 module Codebot
   # This module provides methods for formatting outgoing IRC messages.
   module Formatters
-    # Formats an IRC message.
+    # Formats IRC messages for an event.
     #
     # @param event [Symbol] the webhook event
     # @param payload [Object] the JSON payload object
     # @param color [Boolean] whether to use formatting codes
-    # @return [String] the formatted message
+    # @return [Array<String>] the formatted messages
     def self.format(event, payload, color = true)
-      message = "\x0F" + format_color(event, payload)
-      if color
-        message
-      else
-        ::Cinch::Formatting.unformat message
-      end
+      messages = format_color(event, payload)
+      messages.map! { |msg| "\x0F" + msg }
+      messages.map! { |msg| ::Cinch::Formatting.unformat(msg) } unless color
+      messages
     end
 
-    # Formats a colored IRC message. This method should not be called directly
+    # Formats colored IRC messages. This method should not be called directly
     # from outside this module.
     #
     # @param event [Symbol] the webhook event
     # @param payload [Object] the JSON payload object
-    # @return [String] the formatted message
+    # @return [Array<String>] the formatted messages
     def self.format_color(event, payload)
       case event
       when :ping then Formatters::Ping.new(payload).format
