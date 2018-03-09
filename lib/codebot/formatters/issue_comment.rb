43cb350b920c7320edc470f6af586661c7b1daba
Janik Rabe <info@janikrabe.com>
2018-03-09 07:42:09 +0200

    Add support for issue_comment events

---
+++ lib/codebot/formatters/issue_comment.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Codebot
+  module Formatters
+    # This class formats issue_comment events.
+    class IssueComment < Formatter
+      # Formats IRC messages for an issue_comment event.
+      #
+      # @return [Array<String>] the formatted messages
+      def format
+        ["#{summary}: #{format_url url}"]
+      end
+
+      def summary
+        short = comment_body.lines.first[0..199].strip
+        short << '...' unless short.eql? comment_body.strip
+        "[#{format_repository repository_name}] #{format_user sender_name} " \
+        "commented on issue \##{issue_number}: #{short}"
+      end
+
+      def summary_url
+        extract(:comment, :html_url)
+      end
+
+      def comment_body
+        extract(:comment, :body).to_s
+      end
+
+      def issue_number
+        extract(:issue, :number)
+      end
+    end
+  end
+end
+
+# action
