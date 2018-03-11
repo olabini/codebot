28af19590d9758bb2d03b88c6b1ac182d99d0c4a
Janik Rabe <35016218+janikrabe@users.noreply.github.com>
2018-03-11 07:38:17 +0200

    Add CODEBOT_BIND and CODEBOT_PORT environment variables

--- lib/codebot/web_server.rb
+++ lib/codebot/web_server.rb
@@ -2,12 +2,15 @@
 
 require 'codebot/thread_controller'
 require 'codebot/web_listener'
+require 'codebot/user_error'
 
 require 'sinatra'
 
 module Codebot
   # This class manages a {WebListener} that runs in a separate thread.
   class WebServer < ThreadController
+    extend Sanitizers
+
     # Creates a new web server.
     #
     # @param core [Core] the bot this server belongs to
@@ -20,6 +23,22 @@ module Codebot
       create_server.run!
     end
 
+    # Creates a +Proc+ for configuring this web server.
+    #
+    # @return [Proc] the proc
+    def self.configuration
+      server = self
+      proc do
+        disable :traps
+        set :bind, ENV['CODEBOT_BIND'] if ENV['CODEBOT_BIND'].is_a? String
+        port = ENV['CODEBOT_PORT']
+        if port.is_a?(String) && !port.empty?
+          set :port, (server.valid! port, server.valid_port(port), nil,
+                                    invalid_error: 'invalid port %s')
+        end
+      end
+    end
+
     private
 
     # Creates a new Sinatra server for handling incoming requests.
@@ -30,7 +49,7 @@ module Codebot
       Sinatra.new do
         include WebListener
 
-        configure { disable :traps }
+        configure { instance_eval(&WebServer.configuration) }
         post('/*') { handle_post(core, request, params) }
         error(Sinatra::NotFound) { [405, 'Method not allowed'] }
       end
