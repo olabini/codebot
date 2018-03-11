da09d15b32fb0b2eac5bb2e56d745bf86e20a2f1
Janik Rabe <35016218+janikrabe@users.noreply.github.com>
2018-03-11 07:30:15 +0200

    Change repository URL in metadata.rb

--- lib/codebot/metadata.rb
+++ lib/codebot/metadata.rb
@@ -8,7 +8,7 @@ module Codebot
   VERSION = '1.0.2'.freeze
 
   # The project website.
-  WEBSITE = 'https://github.com/janikrabe/codebot'.freeze
+  WEBSITE = 'https://git.io/codebot'.freeze
 
   # The URL to report issues with a message formatter to.
   FORMATTER_ISSUE_URL = 'https://git.io/formatter-issue'.freeze
