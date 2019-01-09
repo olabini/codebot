950b47e2d5f006c58a35ab150bd7da58ae5dda8a
Janik Rabe <info@janikrabe.com>
2019-01-09 17:19:50 +0200

    Release version 1.1.4

--- lib/codebot/metadata.rb
+++ lib/codebot/metadata.rb
@@ -5,7 +5,7 @@ module Codebot
   PROJECT = 'Codebot'.freeze
 
   # The current project version.
-  VERSION = '1.1.3'.freeze
+  VERSION = '1.1.4'.freeze
 
   # The project website.
   WEBSITE = 'https://git.io/codebot'.freeze
