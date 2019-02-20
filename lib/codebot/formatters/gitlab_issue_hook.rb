0bc5e53662576e1a7be76d8c67ff10784b455e90
Ola Bini <ola@olabini.se>
2019-02-20 12:50:06 -0500

    Make the gitlab push thingy work a bit better

--- lib/codebot/formatters/gitlab_issue_hook.rb
+++ lib/codebot/formatters/gitlab_issue_hook.rb
@@ -8,6 +8,70 @@ module Codebot
         # This needs a bit more work, since Gitlab issue hooks are slightly more complicated than
         # the ones for Github.
 
+
+        #     if(req.body["object_attributes"]["action"] == "update") return;
+
+        #     switch(req.body["object_attributes"]["action"].toLowerCase()) {
+
+        #         case "open":
+        #         var type = "Issue opened by ";
+        #         break;
+
+        #         case "close":
+        #         var type = "Issue closed by ";
+        #         break;
+
+        #         case "reopen":
+        #         var type = "Issue reopened by ";
+        #         break;
+
+        #     }
+
+        #     var service = "Gitlab";
+        #     var issue_id = req.body["object_attributes"]["iid"];
+        #     var issue_title = req.body["object_attributes"]["title"];
+        #     var issue_user = req.body["user"]["name"];
+        #     var issue_url = req.body["object_attributes"]["url"];
+
+
+        # } else if (req.headers["x-github-event"]) {
+
+        #     switch(req.body["action"].toLowerCase()) {
+
+        #         case "opened":
+        #         var type = "Issue opened by ";
+        #         break;
+
+        #         case "closed":
+        #         var type = "Issue closed by ";
+        #         break;
+
+        #         case "reopened":
+        #         var type = "Issue reopened by ";
+        #         break;
+
+        #     }
+
+        #     var service = "Github";
+        #     var issue_id = req.body["issue"]["number"];
+        #     var issue_title = req.body["issue"]["title"];
+        #     var issue_user = req.body["issue"]["user"]["login"];
+        #     var issue_url = req.body["issue"]["html_url"];
+
+        # }
+
+        # for (var channel of channels) {
+
+        #     bot.say(channel, util.format("\x02\x0306Issue\x03\x02: \x02#%d\x02 \x02\x0303%s\x03\x02 - %s%s - %s",
+        #         issue_id,
+        #         issue_title,
+        #         type,
+        #         issue_user,
+        #         issue_url));
+
+        # }
+
+        
         # Formats IRC messages for an issue event.
         #
         # @return [Array<String>] the formatted messages
--- lib/codebot/formatters/gitlab_push_hook.rb
+++ lib/codebot/formatters/gitlab_push_hook.rb
@@ -1,33 +1,35 @@
 # coding: utf-8
-# frozen_string_literal: false
-
-# Portions (c) 2008 Logical Awesome, LLC (released under the MIT license).
-# See the LICENSE file for the full MIT license text.
 
 module Codebot
   module Formatters
     module Gitlab
-      class PushHook < Formatter # rubocop:disable Metrics/ClassLength
+      class PushHook < Formatter
         def format
           repo_url = extract(:repository, :homepage)
           repo_name = extract(:repository, :name)
           user_name = extract(:user_name)
           commits_count = extract(:total_commits_count).to_i
           branch = extract(:ref).split("/")[2..-1].join("/")
-          commit_name = "name"
-          reply = "\x02\x0306Commit\x03\x02: \x02\x0303%s\x03\x02 - %s pushed %d new commit%s to branch \x02%s\x02:" % [
-            repo_name,
-            user_name,
-            commits_count,
-            commits_count == 1 ? "" : "s",
-            branch]
+
+          compare_url = shorten_url "#{repo_url}/compare/#{extract(:before)}...#{extract(:after)}"
+          
+          reply = "[%s]%s pushed #{format_number commits_count, 'new commit', 'new commits'} to %s: %s" % [
+            format_repository(repo_name),
+            format_user(user_name),
+            format_branch(branch),
+            compare_url
+          ]
           
           [reply] + extract(:commits).map do |commit|
-            "\t\x02\x0306～\x03 %s\x02: %s (\x02%s\x02)" % [
-              commit["id"][0..7],
-              commit["message"].gsub(/[\r\n]/, ''),
-              commit["author"][commit_name],
-            ]
+            author = commit['author']['name'] if commit['author'].is_a? Hash
+            '%<repository>s/%<branch>s %<hash>s %<author>s: %<title>s' % {
+              repository: repo_name,
+              branch: format_branch(branch),
+              hash: format_hash(commit['id']),
+              author: format_user(author),
+              title: prettify(commit["message"]),
+              ,
+            }
           end
         end
       end
